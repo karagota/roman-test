009700a862aa2dbdb30e3f8b9c53123a48810990
Olga Butygina <karagota@gmail.com>
2022-12-20 14:21:18 +0300

    store json to database, return id, time and memory

---
+++ app/Http/Controllers/JsonStoreController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JsData;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class JsonStoreController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+    }
+
+
+    public function storeJson(Request $request) {
+        $jsData = JsData::create(['data' => $request->input('data')]);
+        return json_encode(['id'=>$jsData->id,'memory'=>round(memory_get_peak_usage() / 1024)." KB",'time'=>round(microtime(true) - LARAVEL_START).' sec']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/json-store', [App\Http\Controllers\JsonStoreController::class, 'storeJson'])->middleware('auth:sanctum');
+Route::post('/json-store', [App\Http\Controllers\JsonStoreController::class, 'storeJson'])->middleware('auth:sanctum');
